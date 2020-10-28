5d387cefa91c7d60b040f24cbdd850de8989dc79
cribibi <cricler_bianca_melania@yahoo.com>
2020-10-28 14:56:26 +0200

    git test

--- src/Scrabble.java
+++ src/Scrabble.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 public class Scrabble {
-    //testing git
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please insert a number of words");
